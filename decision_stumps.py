79d26fc16a480a8479ae08715b947f44510bbb25
JasonYRChen <jkmylife@gmail.com>
2024-04-13 17:00:36 +0800

    (decision_stumps): correct decision stump algorithm

--- decision_stumps.py
+++ decision_stumps.py
@@ -3,140 +3,111 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 
 
-def decision_stumps(data: list[list[float]], labels: list[int], 
-        distributions: list[float], positive_first: bool = True):
+def decision_stump(X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray=None):
     """
-        Find the minimum loss on every dimension of data and return the
-        dimension and position of the stump and the corresponding loss.
+        Return the optimal choice of feature, stump and polarity to classify
+        the data X with sample weight by decision stump.
 
         paras:
-          data: n x d array-like, the data to learn.
-          labels: n x 1 array of 1 or -1, specifying the category of 
-            corresponding data.
-          distributions: n x 1 array of [0, 1] floats, pointing out the
-            probability of each data.
-          positive_first: bool, True if it's 1 in labels to find first,
-            False otherwise.
+          X: np.ndarray, n x d matrix-like data with n elements and d features.
+          y: np.ndarray, n labels of {1, -1}.
+          sample_weight: np.ndarray, n sample weights. If None is provided, an
+            uniform distribution is implemented.
 
         returns:
-          best_dim: int, the dimension to place stump.
-          best_stump: float, the coordinate along best_dim.
-          best_loss: float, the minimum loss along the learning.
+          best_feature: int, the optimal choice of feature which is in [0, d).
+          best_stump: float, the stump to divide the data in two groups.
+          polarity: int, to classify the two groups which is either {1, -1}.
     """
 
-    if not isinstance(data, np.ndarray):
-        data = np.array(data)
-    if not isinstance(labels, np.ndarray):
-        labels = np.array(labels)
-    if not isinstance(distributions, np.ndarray):
-        distributions = np.array(distributions)
+    rows, cols = X.shape
+    if sample_weight is None:
+        sample_weight = np.ones(rows)
 
-    # how many tokens should visit on each dimension
-    tokens_to_visit = sum(labels > 0 if positive_first else labels < 0)
-    # the basic loss of not finding all the tokens before learning
-    loss = sum(distributions[labels > 0 if positive_first else labels < 0])
-    best_loss, best_dim, best_stump = float('inf'), 0, 0
-    rows, cols = data.shape
-    sign = 1 if positive_first else -1
+    best_feature, best_stump, polarity = None, None, 1 # initialization
+    loss_best = np.inf 
+    loss_pos = sum(sample_weight[y == 1])
+    loss_neg = sum(sample_weight[y == -1])
+    nodes = np.empty(rows + 2)
     for col in range(cols):
-        remain_tokens = tokens_to_visit
-        remain_loss = loss
-        sorted_arg = data[:, col].argsort()
-        prev_pos = data[sorted_arg[0], col] - 1
-        for arg in sorted_arg:
-            # check loss and remained tokens from last loop
-            current_pos = data[arg, col]
-            if remain_loss < best_loss:
-                best_loss = remain_loss
-                best_dim = col
-                best_stump = (prev_pos + current_pos) / 2
-            if not remain_tokens:
-                # all the tokens are found in this dimension. Further search
-                # could only increase loss, so stop here
-                break
-
-            # renew loss and remained tokens with latest label
-            label = labels[arg]
-            odd = distributions[arg]
-            remain_loss -= sign * label / abs(label) * odd
-            if sign * label > 0:
-                remain_tokens -= 1
-            prev_pos = current_pos
-        else:
-            # this can only happen if the wanted token is at the very last
-            # position in 'sorted_arg'. Another check on loss is then needed.
-            current_pos = data[arg, col] + 1
-            if remain_loss < best_loss:
-                best_loss = remain_loss
-                best_dim = col
-                best_stump = (prev_pos + current_pos) / 2
-    return best_dim, best_stump, best_loss
+        loss_p, loss_n = loss_pos, loss_neg
+        indices = np.argsort(X[:, col])
+        nodes[1:-1] = X[indices, col]
+        nodes[0], nodes[-1] = nodes[1] - 1, nodes[-2] + 1
+        for i in range(rows + 1):
+            # renew loss_best
+            if min(loss_pos, loss_neg) < loss_best:
+                best_feature = col
+                best_stump = (nodes[i] + nodes[i+1]) / 2
+                loss_best = min(loss_pos, loss_neg)
+                polarity = 1 if loss_pos < loss_neg else -1
+                
+            # calculate loss_pos and loss_neg
+            if i < rows:
+                index = indices[i]
+                loss_pos -= y[index] * sample_weight[index]
+                loss_neg += y[index] * sample_weight[index]
+    return best_feature, best_stump, polarity
 
 
-def decision_stumps_classifier(data: list[list[float]], labels: list[int], 
-        distributions: list[float], positive_first: bool = True):
-
+def decision_stump_classifier(X: np.ndarray, y: np.ndarray, 
+        sample_weight: np.ndarray=None):
     """
-        Output classified labels with decision stump algorithm.
+        Return the optimal classification by decision stump.
 
         paras:
-          data: n x d array-like, the data to learn.
-          labels: n x 1 array of 1 or -1, specifying the category of 
-            corresponding data.
-          distributions: n x 1 array of [0, 1] floats, pointing out the
-            probability of each data.
-          positive_first: bool, True if it's 1 in labels to find first,
-            False otherwise.
+          X: np.ndarray, n x d matrix-like data with n elements and d features.
+          y: np.ndarray, n labels of {1, -1}.
+          sample_weight: np.ndarray, n sample weights. If None is provided, an
+            uniform distribution is implemented.
 
-        return:
-          new_labels: n x 1 array of 1 and -1, a hypothesis of the labels
-            of data.
+        returns:
+          y_hat: np.ndarray, n classified labels in {1, -1} by decision stump.
     """
 
-    sign = 1 if positive_first else -1
-    dim, stump, _ = decision_stumps(data, labels, distributions, positive_first)
-    new_labels = np.where(data[:, dim] < stump, sign, -sign)
-    return new_labels
+    feature, stump, polarity = decision_stump(X, y, sample_weight)
+    y_hat = np.where(X[:, feature] < stump, polarity, -polarity)
+    return y_hat
 
     
 if __name__ == '__main__':
     sign = 1 # 1 or -1
-#    data = (np.random.rand(100, 5) - 0.5) * 100
-    data = (np.random.rand(100, 2) - 0.5) * 100
+    data_number = 100
+    diameter = 20
+
+    data = (np.random.rand(data_number, 2) - 0.5) * 100
     # spread in x direction
     data[:, 0].sort()
-    labels = np.where(data[:, 0] > -1, -1 * sign, 1 * sign)
+    labels = np.where(data[:, 0] > -10, -1 * sign, 1 * sign)
     # spread in y direction
 #    data[:, 1].sort()
-#    labels = np.where(data[:, 1] > 1, -1 * sign, 1 * sign) # y-direction
+#    labels = np.where(data[:, 1] > 1, -sign, sign) # y-direction
 
-    np.random.shuffle(labels[40:60])
+    # mess with partial labels
+    np.random.shuffle(labels[data_number//2-diameter:data_number//2+diameter])
+
     distributions = np.random.rand(data.shape[0])
 #    distributions = np.full(data.shape[0], 1/data.shape[0])
-    dim, stump, loss = decision_stumps(data, labels, distributions,True if sign > 0 else False )
-    print(f'Split dimension: {dim}, stump position: {stump}, loss: {loss}')
-
+    
+    # original data
+    plt.figure('original')
     colors = np.where(labels > 0, 'b', 'r')
-    sizes = distributions * 100
-#    f = plt.figure(1)
-#    plt.scatter(data[:, 0], data[:, 1])
-    g = plt.figure(1)
-    g.suptitle('Original data with stump')
-    plt.scatter(data[:, 0], data[:, 1], c=colors, s=sizes)
-    if dim:
-        stump_plot = np.array([[min(data[:, 0]), stump], [max(data[:, 0]), stump]])
-        labels_guess = np.where(data[:, 1] > stump, -1 * sign, 1 * sign)
-    else:
-        stump_plot = np.array([[stump, min(data[:, 1])], [stump, max(data[:, 1])]])
-        labels_guess = np.where(data[:, 0] > stump, -1 * sign, 1 * sign)
-    plt.plot(stump_plot[:, 0], stump_plot[:, 1], c='g')
-    h = plt.figure(2)
-    h.suptitle('Classified data by stump')
-    colors_new = np.where(labels_guess > 0, 'b', 'r')
-    plt.scatter(data[:, 0], data[:, 1], c=colors_new, s=sizes)
-    plt.plot(stump_plot[:, 0], stump_plot[:, 1], c='g')
+    plt.scatter(data[:, 0], data[:, 1], c=colors, s=distributions*100)
 
-    plt.figure()
-    color_guess = np.where(decision_stumps_classifier(data, labels, distributions, True if sign > 0 else False) > 0, 'b', 'r')
-    plt.scatter(data[:, 0], data[:, 1], c=color_guess, s=sizes)
+    # decision stump classifier
+    y = decision_stump_classifier(data, labels, distributions)
+    colors = np.where(y > 0, 'b', 'r')
+    plt.figure('decision stump classifer')
+    plt.scatter(data[:, 0], data[:, 1], c=colors, s=distributions*100)
+
+    # decision stump
+    f, s, p = decision_stump(data, labels, distributions)
+    y = np.where(data[:, f] < s, p, -p)
+    plt.figure('decision stump')
+    colors = np.where(y > 0, 'b', 'r')
+    plt.scatter(data[:, 0], data[:, 1], c=colors, s=distributions*100)
+    xx, yy = ([-50, 50], [s, s]) if f else ([s, s], [-50, 50])
+    plt.plot(xx, yy, 'g')
+
+
     plt.show()
